c6ba1df316c65962f99ff22af6a48798c68fc4ee
David Rönnlid - Nootralize <dr@nootralize.com>
2022-01-14 15:55:57 +0100

    Removed js suffix when fetching serverless fnction

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -7,7 +7,7 @@ const initialState = {
 }
 
 export const fetchMovies = createAsyncThunk('movies/fetchMovies', async () => {
-  fetch('https://davidronnlidmovies.com/.netlify/functions/api.js').then(
+  fetch('https://davidronnlidmovies.netlify.app/.netlify/functions/api').then(
     (response) => {
       console.log(response)
       return response
