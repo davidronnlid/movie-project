ab9b7a633fd7fb93f235f60a7095f0d6c8de829b
David Rönnlid - Nootralize <dr@nootralize.com>
2022-01-26 17:47:48 +0100

    Commented out unused var

--- src/features/movies/MoviesList.js
+++ src/features/movies/MoviesList.js
@@ -34,7 +34,7 @@ export const MoviesList = () => {
   const movies = useSelector(selectAllMovies)
 
   const movieStatus = useSelector((state) => state.movies.status)
-  const error = useSelector((state) => state.movies.error)
+  // const error = useSelector((state) => state.movies.error)
 
   const [showMovCar, setShowMovCar] = useState(true)
   const toggleshowMovCar = () => setShowMovCar((showMovCar) => !showMovCar)
