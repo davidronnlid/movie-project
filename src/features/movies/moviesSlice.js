9dfc00d5bf32b0b362cf9158c071b12fe1f01755
David Rönnlid - Nootralize <dr@nootralize.com>
2022-02-08 15:58:27 +0100

    Logging new stuff

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -44,7 +44,8 @@ const moviesSlice = createSlice({
 
 export default moviesSlice.reducer
 
-export const selectAllMovies = (state) => state.movies.movies[0].data.results
+export const selectAllMovies = (state) => state.movies.movies[0]
 
 export const selectMovieById = (state, movieId) =>
+  console.log(state.movies) &&
   state.movies.find((movie) => movie.id.toString() === movieId)
