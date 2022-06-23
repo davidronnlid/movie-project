c6652e4a9f4fa4f73cb068b58e9c75691a2b8e57
David Rönnlid <david.ronnlid@gmail.com>
2022-06-23 08:47:35 +0200

    See prev commit

--- src/features/movies/moviesCarousel.js
+++ src/features/movies/moviesCarousel.js
@@ -14,7 +14,7 @@ const flickityOptions = {
 }
 
 export default function MoviesCarousel(movies) {
-  console.log("received this data in MoviesCarousel component")
+  console.log("received this data in MoviesCarousel component", movies)
   return (
     <>
       {useWindowSize().width < 768 ? (
